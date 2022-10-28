40ebcb2ba5a3ec40c10815ffd4d6685c225d8b36
white <white@example.com>
2022-10-28 17:47:21 +0500

    greet white

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+
+        static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
+    }
+}
